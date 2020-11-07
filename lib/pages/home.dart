7992506fbb5f43741fa988e861968b7806509007
carlandrian <carlandrian@gmail.com>
2020-11-07 08:30:38 +0800

    navigation using PageView

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/activity_feed.dart';
+import 'package:fluttershare/pages/profile.dart';
+import 'package:fluttershare/pages/search.dart';
+import 'package:fluttershare/pages/timeline.dart';
+import 'package:fluttershare/pages/upload.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -7,14 +16,104 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool isAuth = false;
+  PageController pageController;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+    // Detects when a user signed-in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignin(account);
+    }, onError: (err){
+      print('Error signing in: $err');
+    });
+    // Re-authenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false)
+      .then((account) {
+        handleSignin(account);
+    }).catchError((err) {
+      print('Error signing in: $err');
+    });
+  }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  handleSignin(GoogleSignInAccount account) {
+    if(account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.jumpToPage(
+      pageIndex
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return isAuth ? buildAuthScreen() : buildUnauthScreen();
   }
 
-  Widget buildAuthScreen() {
-    return Text('Authenticated');
+  Scaffold buildAuthScreen() {
+    return Scaffold(
+      body: PageView(
+        children: [
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),  // This will not scroll the PageView as we only want the views inside of it to scroll.
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.whatshot)),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_active)),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_camera, size: 35.0,)),
+          BottomNavigationBarItem(icon: Icon(Icons.search)),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle)),
+        ],
+      ),
+    );
+    // return RaisedButton(
+    //     onPressed: logout,
+    //     child: Text(
+    //       'Logout'
+    //     ),
+    // );
   }
 
   Scaffold buildUnauthScreen() {
@@ -43,7 +142,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             GestureDetector(
-              onTap: () => print('tapped'),
+              onTap: login,
               child: Container(
                 width: 260.0,
                 height: 60.0,
