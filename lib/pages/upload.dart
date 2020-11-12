2efd03a1d86ebe324650f9d98966b22e37f44766
Carl Canotal <carlcanotal@Carls-MacBook-Air.local>
2020-11-12 21:37:47 +0800

    Building Upload Form

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,8 +10,91 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  File file;
+
+  handleTakePhoto() async{
+    Navigator.pop(context);
+    PickedFile pickedFile = await ImagePicker().getImage(
+        source: ImageSource.camera,
+      maxHeight: 675,
+      maxWidth: 960,
+    );
+    File file = File(pickedFile.path);
+
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  handleChooseFromGallery() async {
+    Navigator.pop(context);
+    PickedFile pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+    File file = File(pickedFile.path);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+        context: parentContext,
+      builder: (context) {
+          return SimpleDialog(
+            title: Text("Create Post"),
+            children: [
+              SimpleDialogOption(
+                child: Text("Photo with Camera"),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text("Image from Gallery"),
+                onPressed: handleChooseFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+      }
+    );
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+        color: Theme.of(context).accentColor.withOpacity(0.6),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset('assets/images/upload.svg', height: 260.0,),
+            Padding(
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  "Upload Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                  ),
+                ),
+                color: Colors.deepOrange,
+                onPressed: () => selectImage(context),
+              ),
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+          ]
+        )
+    );
+  }
+
+  buildUploadForm() {
+    Text("File loaded");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Upload');
+    return file == null ? buildSplashScreen() : buildUploadForm();
   }
 }
