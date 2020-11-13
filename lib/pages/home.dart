467253d05939e275256f82c1a85c09615b6b4d78
carlandrian <carlandrian@gmail.com>
2020-11-13 16:21:10 +0800

    Uploading Posts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/models/user.dart';
@@ -12,6 +13,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final Query usersRef = FirebaseFirestore.instance.collection('users');
+final Query postsRef = FirebaseFirestore.instance.collection('posts');
+final Reference storageRef  = FirebaseStorage.instance.ref();
 final DateTime timestamp = DateTime.now();
 User currentUser;
 
@@ -136,7 +139,7 @@ class _HomeState extends State<Home> {
             child: Text("Logout"),
           ),
           ActivityFeed(),
-          Upload(),
+          Upload(currentUser: currentUser),
           Search(),
           Profile(),
         ],
--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,16 +1,33 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as Im;
+import 'package:uuid/uuid.dart';
 
 class Upload extends StatefulWidget {
+  final User currentUser;
+
+  Upload({this.currentUser});
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
+  TextEditingController captionController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
   File file;
+  bool isUploading = false;
+  String postId = Uuid().v4();
 
   handleTakePhoto() async{
     Navigator.pop(context);
@@ -89,8 +106,201 @@ class _UploadState extends State<Upload> {
     );
   }
 
+  clearImage() {
+    setState(() {
+      file = null;
+    });
+  }
+
+  compressImage() async {
+    final tempDir = await getTemporaryDirectory();
+    final path = tempDir.path;
+    Im.Image imageFile = Im.decodeImage(file.readAsBytesSync());
+    final compressedImageFile = File('$path/img_$postId.jpg')
+      ..writeAsBytesSync(Im.encodeJpg(imageFile, quality: 85));
+
+    setState(() {
+      file = compressedImageFile;
+    });
+  }
+
+  Future<String> uploadImage(imageFile) async {
+    UploadTask uploadTask = storageRef.child("post_$postId.jpg").putFile(imageFile);
+
+    // TODO: Review this implementation of TaskSnapshot, there might be a better way to implement.
+    TaskSnapshot storageSnap = await uploadTask.whenComplete(() => {
+
+    });
+    if(storageSnap.state == TaskState.success) {
+      // print(storageSnap);
+      String downloadUrl = await storageSnap.ref.getDownloadURL();
+      return downloadUrl;
+    }
+  }
+
+  createPostInFireStore({String mediaUrl, String location, String description}) {
+    postsRef.firestore
+        .collection('users')
+        .doc(widget.currentUser.id)
+        .collection('userPosts')
+        .doc(postId)
+        .set({
+      "postId": postId,
+      "ownerId": widget.currentUser.id,
+      "username": widget.currentUser.username,
+      "mediaUrl": mediaUrl,
+      "description": description,
+      "location": location,
+      "timestamp": timestamp,
+      "likes": {},
+    });
+
+
+  }
+
+  handleSubmit() async {
+    setState(() {
+      isUploading = true;
+    });
+    await compressImage();
+    String mediaUrl = await uploadImage(file);
+    createPostInFireStore(
+      mediaUrl: mediaUrl,
+      location: locationController.text,
+      description: captionController.text,
+    );
+
+    captionController.clear();
+    locationController.clear();
+
+    setState(() {
+      file = null;
+      isUploading = false;
+      postId = Uuid().v4();
+    });
+  }
+
   buildUploadForm() {
-    Text("File loaded");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: clearImage,
+        ),
+        title: Text(
+          "Caption Post",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          FlatButton(
+              onPressed: isUploading ? null : () => handleSubmit(), //  null set the flatbutton disabled
+              child: Text(
+                "Post",
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                ),
+              ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          isUploading ? linearProgress() :Text(""),
+          Container(
+            height: 220.0,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(file),
+                    )
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(widget.currentUser.photoUrl),
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                controller: captionController,
+                decoration: InputDecoration(
+                  hintText: "Write a caption...",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.pin_drop, color: Colors.orange, size: 35.0,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                controller: locationController,
+                decoration: InputDecoration(
+                  hintText: "Where was this photo taken?",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 200.0,
+            height: 100.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+                onPressed: getUserLocation,
+                icon: Icon(
+                  Icons.my_location,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  "Use Current Location",
+                  style: TextStyle(
+                    color: Colors.white,
+
+                  ),
+                ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.blue,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+    String completeAddress = '${placemark.subThoroughfare} ${placemark.thoroughfare} ${placemark.subLocality} '
+        '${placemark.locality} ${placemark.subAdministrativeArea} ${placemark.administrativeArea} '
+        '${placemark.postalCode} ${placemark.country}';
+    print(completeAddress);
+
+    String formattedAddress = '${placemark.locality}, ${placemark.country}';
+    locationController.text = formattedAddress;
   }
 
   @override
